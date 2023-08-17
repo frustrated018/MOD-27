f03d9822f929df6b7631dec5f4a8552e2bfbd087
frustrated018 <niloyd018@gmail.com>
2023-08-17 16:03:21 +0600

    Made the dashboard & now working on DOM

---
+++ dash.js
@@ -0,0 +1,9 @@
+const depositAmount = document.getElementById('deposit-amount');
+
+
+
+// Deposit button Event handler
+
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    console.log(depositAmount.value);
+})
